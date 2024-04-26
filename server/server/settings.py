6b81d4a8d1d419848ef3cce20bfe4d48708b2d2b
Ks <166847420+y0ukn0wh0@users.noreply.github.com>
2024-04-26 19:27:17 +0000

    feat:db err

--- server/server/settings.py
+++ server/server/settings.py
@@ -105,7 +105,7 @@ else :
     DATABASES = {
     'default': dj_database_url.config(
         # Replace this value with your local database's connection string.
-        default='postgresql://postgres:postgres@localhost:5432/mysite',
+        default='postgresql://postgres:postgres@localhost:5432/mysite_b1ws',
         conn_max_age=600
     )
 }
